8a43ec525a0af73b3ec510789941575201fd87b1
bishalkunwar <72371055+bishalkunwar@users.noreply.github.com>
2021-05-31 21:10:53 +0545

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class App extends Component{
+   render(){
+      return(
+         <div>
+            <h1>Header</h1>
+            <h2>Content</h2>
+            <p>This is the content!!!</p>
+         </div>
+      );
+   }
+}
+export default App;
